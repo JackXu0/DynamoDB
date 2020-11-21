998b2d04ee1016e25491342ab32ffea65909e9a0
JackXu0 <627054436@qq.com>
2020-11-21 03:44:03 -0500

    updated design

--- src/Test.java
+++ src/Test.java
@@ -76,6 +76,9 @@ class VirtualNode {
     int partition;
     ArrayList<KV> storage;
 
+    //TODO: add previous virtual node field
+    //TODO: add previous virtual node hash field
+
     public VirtualNode(Worker worker, int hash, int parttiton){
         this.worker = worker;
         this.hash = hash;
@@ -85,23 +88,80 @@ class VirtualNode {
 
 }
 
+
+// TODO: Add function broadcast (worker_map, msg)
+
+
 class Worker implements Runnable {
+    // List<String> storing membership changing history
+    List<String> membership_changing_history = new ArrayList<>();
+    // List<VirtualNode> storing the ring of virtual nodes
+    private ArrayList<VirtualNode> virtual_nodes_ring;
+    // Map<Worker, Boolean> storing the status for each worker node
+    private Map<Worker,Boolean> worker_map = new HashMap<>();
+    //TODO: Add a field indicating whether this worker is a seed
+
+    //TODO: Add total number of workers, and total number of failed workers
+
+    //TODO: Add a Merkel tree for each partition
+
+    //TODO: Add a field Map<String, String> records storing all key-value pairs
+
     private boolean stop = false;
     private final BlockingQueue<Message> workQueue;
-    private ArrayList<VirtualNode> VNs;
+
+    // Map to a set of tokens (Virtual nodes) on initialization.
+    // TODO: generate two virtual nodes
+    // TODO: broadcast to everyone about the arrival of this worker, and the virtual nodes it chose
+    // TODO: constructor should be Worker(Worker seed)
 
     public Worker(BlockingQueue<Message> workQueue, Integer num_partition, Master ) {
         this.workQueue = workQueue;
-        this.VNs = new ArrayList();
+        this.virtual_nodes_ring = new ArrayList();
         int partition = 0;
         while (num_partition > 0) {
             Random rand = new Random();
             Integer hash = rand.nextInt();
             VirtualNode VN = new VirtualNode(this, hash, num_partition);
-            this.VNs.add(VN);
+            this.virtual_nodes_ring.add(VN);
 
         }
     }
+
+
+
+    //TODO: Handle messages from clients
+
+    //TODO: Get the coordinate node
+
+    //TODO: check whether a particular worker is up or down
+
+    //TODO: handle get/put requests
+
+    //TODO: transfer a partition to another worker (start_hash, end_hash, destination_worker)
+
+    //TODO: received a partition from another worker (records)
+
+    //TODO: Implement a timer to synchronize data with other workers
+
+    //TODO: Function to request synchronization (virtual_nodes_ring, worker)
+    //TODO: Function to request synchronization (membership_changing_history)
+    //TODO: Function to request synchronization (merkel_tree)
+    //TODO: Function to request synchronization (vector_time)
+
+    //TODO: Function to handle response (virtual_nodes_ring, worker_map) -- update merkel tree if necessary
+    //TODO: Function to handle response (membership_changing_history)
+    //TODO: Function to handle response (merkel_tree)
+    //TODO: Function to handle response (vector_time)
+
+    //TODO: Function for Seed Worker:
+    // if received message from an unknown worker,
+    // add the virtual nodes of this worker to virtual_nodes_ring and the worker to worker_map
+    // send to it the new virtual_nodes_ring and new worker_map info
+
+
+
+
 
     public void store(Integer VN_patition, Pair<String, String> message) {
         //this.Storage.get(VN_patition).put(message.first(), message.second());
